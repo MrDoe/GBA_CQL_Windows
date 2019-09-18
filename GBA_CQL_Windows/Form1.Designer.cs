3d4a16bd4a1a91fb8d14a7e589928766c58e1d3e
Christoph Döllinger <christoph.doellinger@med.uni-heidelberg.de>
2019-09-18 14:54:11 +0200

    Projektdateien hinzufügen.

---
+++ GBA_CQL_Windows/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace GBA_CQL_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ddlServer = new System.Windows.Forms.ComboBox();
+            this.ddlResourceType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCQL = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 41);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CQL-Abfrage:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(14, 295);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(71, 28);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Absenden";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 339);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Antwort:";
+            // 
+            // ddlServer
+            // 
+            this.ddlServer.FormattingEnabled = true;
+            this.ddlServer.Items.AddRange(new object[] {
+            "https://blaze.life.uni-leipzig.de/fhir"});
+            this.ddlServer.Location = new System.Drawing.Point(58, 10);
+            this.ddlServer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ddlServer.Name = "ddlServer";
+            this.ddlServer.Size = new System.Drawing.Size(210, 21);
+            this.ddlServer.TabIndex = 6;
+            this.ddlServer.Text = "https://blaze.life.uni-leipzig.de/fhir";
+            // 
+            // ddlResourceType
+            // 
+            this.ddlResourceType.FormattingEnabled = true;
+            this.ddlResourceType.Items.AddRange(new object[] {
+            "Patient",
+            "Specimen",
+            "Observation",
+            "Condition"});
+            this.ddlResourceType.Location = new System.Drawing.Point(100, 262);
+            this.ddlResourceType.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ddlResourceType.Name = "ddlResourceType";
+            this.ddlResourceType.Size = new System.Drawing.Size(120, 21);
+            this.ddlResourceType.TabIndex = 7;
+            this.ddlResourceType.Text = "Patient";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 264);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "FHIR Resource:";
+            // 
+            // txtCQL
+            // 
+            this.txtCQL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCQL.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCQL.Location = new System.Drawing.Point(16, 57);
+            this.txtCQL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCQL.Name = "txtCQL";
+            this.txtCQL.Size = new System.Drawing.Size(686, 200);
+            this.txtCQL.TabIndex = 9;
+            this.txtCQL.Text = "";
+            this.txtCQL.TextChanged += new System.EventHandler(this.txtCQL_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(14, 356);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(686, 326);
+            this.txtOutput.TabIndex = 10;
+            this.txtOutput.Text = "";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 693);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtCQL);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ddlResourceType);
+            this.Controls.Add(this.ddlServer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "GBA CQL Query Tool";
+            this.Load += new System.EventHandler(this.Page_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ddlServer;
+        private System.Windows.Forms.ComboBox ddlResourceType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox txtCQL;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ GBA_CQL_Windows/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace GBA_CQL_Windows
+{
+    public partial class Form1 : Form
+    {
+        public bool bInit;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Content
+        {
+            public string contentType { get; set; }
+            public string data { get; set; }
+        }
+        public class JSON_Library
+        {
+            public string resourceType { get; set; }
+            public List<Content> content { get; set; }
+            public string url { get; set; }
+            public JSON_Library()
+            {
+                resourceType = "Library";
+                content = new List<Content>();
+            }
+        }
+        public class Coding
+        {
+            public string system { get; set; }
+            public string code { get; set; }
+        }
+        public class Scoring
+        {
+            public List<Coding> coding { get; set; }
+            public Scoring()
+            {
+                coding = new List<Coding>
+                {
+                    new Coding { system = "http://terminology.hl7.org/CodeSystem/measure-scoring",
+                                 code = "cohort" }
+                };
+            }
+        }
+        public class Criteria
+        {
+            public string expression { get; set; }
+            public string language { get; set; }
+            public Criteria()
+            {
+                expression = "InInitialPopulation";
+                language = "text/cql";
+            }
+        }
+        public class Code
+        {
+            public List<Coding> coding { get; set; }
+            public Code()
+            {
+                coding = new List<Coding>
+                {
+                    new Coding { system = "http://terminology.hl7.org/CodeSystem/measure-population",
+                                 code = "initial-population" }
+                };
+            }
+        }
+        public class Population
+        {
+            public Criteria criteria { get; set; }
+            public Code code { get; set; }
+            public Population()
+            {
+                criteria = new Criteria();
+                code = new Code();
+            }
+        }
+        public class Group
+        {
+            public List<Population> population { get; set; }
+            public Group()
+            {
+                population = new List<Population> { new Population() };
+            }
+        }
+        public class SubjectCodeableConcept
+        {
+            public List<Coding> coding;
+            public SubjectCodeableConcept(string sType)
+            {
+                coding = new List<Coding> { new Coding { system = "http://hl7.org/fhir/resource-types",
+                                                         code = sType }};
+            }
+        }
+        public class JSON_Measure
+        {
+            public string url { get; set; }
+            public Scoring scoring { get; set; }
+            public List<Group> group { get; set; }
+            public string status { get; set; }
+            public List<string> library { get; set; }
+            public string resourceType { get; set; }
+            public SubjectCodeableConcept subjectCodeableConcept { get; set; }
+            public JSON_Measure(string sType)
+            {
+                scoring = new Scoring();
+                group = new List<Group> { new Group() };
+                status = "active";
+                resourceType = "Measure";
+                subjectCodeableConcept = new SubjectCodeableConcept(sType);
+            }
+        }
+        public class JSON_Requests
+        {
+            public string sMeaID { get; set; }
+            public string sJSON_Lib { get; set; }
+            public string sJSON_Mea { get; set; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!bInit)
+            {
+                txtCQL.Text = "library Retrieve\r\nusing FHIR version '4.0.0'\r\ninclude FHIRHelpers version '4.0.0'\r\n\r\ncontext Patient\r\n\r\ndefine InInitialPopulation:\r\n  Patient.gender = 'male'\r\n";
+                bInit = true;
+            }
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            ProcessData(txtCQL.Text);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        private List<string> CreateLibrary(string b64Input)
+        {
+            string uuid_lib = System.Guid.NewGuid().ToString();
+
+            // set content
+            Content cCQL = new Content { contentType = "text/cql", data = b64Input };
+
+            // create json library object
+            JSON_Library oJson = new JSON_Library
+            {
+                content = { cCQL },
+                url = "urn:uuid:" + uuid_lib
+            };
+
+            return new List<string> { uuid_lib, JsonConvert.SerializeObject(oJson, Formatting.Indented) };
+        }
+        private string CreateMeasure(string uuid_lib)
+        {
+            string sType = (string)ddlResourceType.SelectedValue;
+
+            // create json measure object
+            JSON_Measure oJson = new JSON_Measure(sType)
+            {
+                url = "urn:uuid:" + System.Guid.NewGuid().ToString(),
+                library = new List<string> { "urn:uuid:" + uuid_lib }
+            };
+
+            return JsonConvert.SerializeObject(oJson, Formatting.Indented);
+        }
+        private void ProcessData(string input)
+        {
+            JSON_Requests requests = new JSON_Requests();
+
+            // convert to base64
+            var bInput = System.Text.Encoding.UTF8.GetBytes(input);
+            string b64Input = System.Convert.ToBase64String(bInput);
+
+            // create library
+            List<string> aReturn = CreateLibrary(b64Input);
+            string uuid_lib = aReturn[0];
+            requests.sJSON_Lib = aReturn[1];
+
+            // post to server
+            SendRequest(requests.sJSON_Lib, "Library");
+
+            // create measure
+            requests.sJSON_Mea = CreateMeasure(uuid_lib);
+
+            // post to server
+            string sResponse = SendRequest(requests.sJSON_Mea, "Measure");
+
+            if (sResponse == "-1")
+            {
+                MessageBox.Show("Fehler", "Keine Antwort vom Server!");
+                return;
+            }
+
+            // retrieve response id
+            JObject oMea = JObject.Parse(@sResponse);
+            string uuid_mea = (string)oMea.SelectToken(".id");
+            requests.sMeaID = uuid_mea;
+
+            // send get request to evaluate measure
+            sResponse = SendRequest(null, "Measure/" + requests.sMeaID + "/$evaluate-measure?periodStart=2000&periodEnd=2099");
+
+            // beautify and print JSON output
+            txtOutput.Text = JValue.Parse(sResponse).ToString(Formatting.Indented);
+        }
+        private string SendRequest(string sJSON, string sType)
+        {
+            var client = new RestClient(ddlServer.Text + "/" + sType);
+            var request = new RestRequest();
+
+            if (sJSON == null)
+                request.Method = Method.GET;
+            else
+                request.Method = Method.POST;
+
+            request.AddHeader("content-type", "application/fhir+json");
+            request.Parameters.Clear();
+            request.AddParameter("application/fhir+json", sJSON, ParameterType.RequestBody);
+
+            var response = client.Execute(request);
+
+            if (response.Content.Length > 0)
+                return response.Content;
+            else
+                return "-1"; // no response
+        }
+        protected void txtCQL_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCQL.Text.Contains("context Patient"))
+                ddlResourceType.Text = "Patient";
+            else if (txtCQL.Text.Contains("context Specimen"))
+                ddlResourceType.Text = "Specimen";
+            else if (txtCQL.Text.Contains("codesystem icd10"))
+                ddlResourceType.Text = "Condition";
+        }
+    }
+}
